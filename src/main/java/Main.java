d09a7ac5963411bc3682a8f7eefecf9c6d0cce3c
Shah Raiaan <smraiaan@gmail.com>
2020-07-30 14:12:02 -0400

    log4j2 added
    there is an issue with reading the log4j.properties file which need to be looked at later

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
-
+private static Logger logger = LoggerFactory.getLogger(Main.class);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//System.out.println(System.getProperty("user.dir"));
+		//System.setProperty("log4j.configurationFile", System.getProperty("user.dir")+"/log4j2.properties");
+		//PropertyConfigurator.configure(System.getProperty("user.dir")+"/log4j2.properties");
+		//BasicConfigurator.configure();
+		String message = "______Hello there_____";
+		logger.info("Info--------------- "+message);
+		logger.debug("debug--------------- "+message);
+		logger.error("error--------------- "+message);
 		
 		
 	}
--- src/test/java/org/suborg/project/teamname/runner/CucumberTestRunner.java
+++ src/test/java/org/suborg/project/teamname/runner/CucumberTestRunner.java
@@ -1,21 +1,40 @@
 package org.suborg.project.teamname.runner;
 
 import org.suborg.project.teamname.baseconfig.Driver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class CucumberTestRunner {
 
-	@BeforeClass
+	@BeforeMethod
 	public void setup() {
-		Driver driver = new Driver();
+		 new Driver();
+	}
+	@AfterMethod
+	public void teardown() {
+		 Driver.getDriver().quit();
+		 Driver.getDriver().quit();
 	}
 	
 	@Test
 	public void letsseeifitworks() {
 		System.out.println("------------------it works-------------");
 		Driver.getDriver().get("https://facebook.com");
-		Driver.getDriver().quit();
+		//Driver.getDriver().quit();
+	}
+	@Test
+	public void test2() {
+		System.out.println("------------------test2 works-------------");
+		Driver.getDriver().get("https://facebook.com");
+		//Driver.getDriver().quit();
+	}
+	@Test
+	public void withouttag() {
+		System.out.println("------------------it works-------------");
+		Driver.getDriver().get("https://facebook.com");
+		//Driver.getDriver().quit();
 	}
 	
 }
